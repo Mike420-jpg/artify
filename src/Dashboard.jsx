e81204c47a4623ddb58059be0812b566326f25ed
Mike420-jpg <qmrrnarvarte@tip.edu.ph>
2026-04-09 07:46:41 +0800

    SuperAd and Admin UI

---
+++ src/Dashboard.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './Dashboard.css';
+
+const Dashboard = () => {
+    return (
+        <div className="Dashboard">
+            <div className="main">
+                <h1 className="welcome">Welcome, Admin!</h1>
+
+                    <div className="container">
+                        <div className="RegUsers">
+                            <p>Registered Users</p>
+                        </div>
+
+                    <div className="UploadedArt">
+                        <p>Uploaded Artworks</p>
+                    </div>
+
+                    <div className="Transact">
+                        <p>Transactions</p>
+                    </div>
+            </div>
+
+                <div className="RecentUpload">
+                    <p>Recent Uploaded Artworks</p>
+
+                    <div className="columns">
+                        <p>Artwork</p>
+                        <p>Artist</p>
+                        <p>Price</p>
+                        <p>Status</p>
+                        <p>Uploaded at</p>
+                    </div>
+
+                    <div className="gray-box"></div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Dashboard;
